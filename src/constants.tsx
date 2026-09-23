01d881f596324b5772b753d3db170ca718e35698
Reshma G <reshma.g@cdg.ws>
2026-09-23 12:36:03 +0530

    fix

--- src/constants.tsx
+++ src/constants.tsx
@@ -1,5 +1,5 @@
-export const BACKEND_URL =  "https://payyanur-cl-be-204746249106.europe-west1.run.app/"   //  "https://my-node-app-204746249106.asia-south1.run.app/" //  ; 
-export const TOTAL_PLAYER = 11
+export const BACKEND_URL = "https://payyanur-cl-be-204746249106.europe-west1.run.app/"   //  "https://my-node-app-204746249106.asia-south1.run.app/" //  ; 
+export const TOTAL_PLAYER = 10
 
 
 
